3c290492cb5c9804c7c84ca0f25f6b10f3ed6e94
Chris <clj@ollure.com>
2019-08-21 21:44:54 -0500

    tested js, working previous error due to server not seeing updates

--- main.js
+++ main.js
@@ -4,21 +4,21 @@ const searchIcon = document.getElementById('search-icon');
 const searchBox = document.getElementById('searchbox');
 
 searchIcon.addEventListener('click', function () {
-	if(searchBox.style.top == '72px') {
-		searchBox.style.top = '24px';
-		searchBox.style.pointerEvents = 'none';
+	if (searchBox.style.top == '72px') {
+	 searchBox.style.top = '24px';
+	 searchBox.style.pointerEvents = 'none';
 	} else {
-		searchBox.style.top = '72px';
-		searchBox.style.pointerEvents = 'auto';	
+	 searchBox.style.top = '72px';
+	 searchBox.style.pointerEvents = 'auto';	
 	}
 });
 
 menuIcon.addEventListener('click', function() {
-	if(slideoutMenu.style.opacity  == '1') {
-	slideoutMenu.style.opacity = '0';
-	slideoutMenu.style.pointerEvents = 'none';
+	if (slideoutMenu.style.opacity  == "1") {
+	 slideoutMenu.style.opacity = '0';
+	 slideoutMenu.style.pointerEvents = 'none';
 	} else {
-	slideoutMenu.style.opacity = '1';
-	slideoutMenu.style.pointerEvents = 'none';
+	 slideoutMenu.style.opacity = '1';
+	 slideoutMenu.style.pointerEvents = 'none';
 	}
 })
